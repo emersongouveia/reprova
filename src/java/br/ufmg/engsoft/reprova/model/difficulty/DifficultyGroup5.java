6a9436ecf3343d31acd5138e9f1cc6be011226b1
Celso Junio Simões de Oliveira Santos <celsos@ciandt.com>
2022-11-22 15:57:36 -0300

    Creating new Class; Improving code; Improving Cyclomatic complexity and Duplicated Code

--- src/java/br/ufmg/engsoft/reprova/model/difficulty/DifficultyGroup5.java
+++ src/java/br/ufmg/engsoft/reprova/model/difficulty/DifficultyGroup5.java
@@ -7,9 +7,9 @@ public class DifficultyGroup5 implements IDifficultyGroup{
 
   public int getDifficultyGroup(double avg){
     if (avg < 20) {return 0;}
-    if (avg < 40) {return 1;}
-    if (avg < 60) {return 2;}
-    if (avg < 80) {return 3;}
+    else if (avg < 40) {return 1;}
+    else if (avg < 60) {return 2;}
+    else if (avg < 80) {return 3;}
     return 4;
   };
 
